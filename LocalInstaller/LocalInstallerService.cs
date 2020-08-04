3970c37e041962ccb1b203ef0372791f2f54d76d
Christian M Macy <christian.macy@opsysinc.com>
2020-08-03 19:13:30 -0600

    DT467: Pass PT Plugin Path to Constructor
    The `LocalInstallerService` is now instantiated with the
    path where ParaText plugins are installed.
    
    Side Effects:
    * Major cleanup in the `LocalInstallerService` class removes extra usings and
    properties, as well as updating some docs
    * Removes `FileName` from `PluginDescription`

--- LocalInstaller/LocalInstallerService.cs
+++ LocalInstaller/LocalInstallerService.cs
@@ -3,10 +3,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Collections.Generic;
 using Newtonsoft.Json;
-using Microsoft.Win32;
-using System.Diagnostics;
 using PpmMain.Models;
-using PpmMain.Util;
 
 namespace PpmMain.LocalInstaller
 {
@@ -18,127 +15,82 @@ namespace PpmMain.LocalInstaller
     }
 
     /// <summary>
-    /// Handles management and installation of local plugins
+    /// Handles management and installation of local plugins.
     /// </summary>
     public class LocalInstallerService : ILocalInstallerService
     {
-        private readonly string ptInstalledPluginsDirectory = "plugins";
-        private readonly string ppmDirectory = "ParatextPluginManagerPlugin";
-        private readonly string ppmInstalledPluginDataDirectory = "plugins";
-        private readonly string ppmDownloadedPluginsDirectory = "downloads";
-        private string ptInstallationPath;
-        private string ppmPath;
-
-        public LocalInstallerService()
-        {
-            ptInstallationPath = GetPTInstallationPath();
-            ppmPath = $"{ptInstallationPath}\\{ptInstalledPluginsDirectory}\\{ppmDirectory}";
-        }
         /// <summary>
-        /// Gets descriptions of the currently installed plugins
+        /// The directory name where ParaText plugins are installed.
+        /// </summary>
+        private string PtInstalledPluginsPath { get; set; }
+
+        public LocalInstallerService(string ptInstalledPluginsPath)
+        {
+            PtInstalledPluginsPath = ptInstalledPluginsPath ?? throw new ArgumentNullException(nameof(ptInstalledPluginsPath));
+        }
+
+        /// <summary>
+        /// Gets descriptions of the currently installed plugins.
         /// </summary>
         /// <returns></returns>
         public List<PluginDescription> GetInstalledPlugins()
         {
-            return GetPluginDescriptions($"{ppmPath}\\{ppmInstalledPluginDataDirectory}");
+            return GetPluginDescriptions(PtInstalledPluginsPath);
         }
+
         /// <summary>
-        /// Installs a ParaText plugin
+        /// Installs a ParaText plugin.
         /// </summary>
-        /// <param name="plugin"></param>
-        /// <param name="pluginArchive"></param>
+        /// <param name="pluginArchive">The zip file containing the plugin data.</param>
         public void InstallPlugin(FileInfo pluginArchive)
         {
-            if (NeedsElevatedPermissions())
-                ElevatePermissions();
-
-            try
-            {
-                string zipFilePath = pluginArchive.FullName;
-                string jsonFilePath = Path.ChangeExtension(zipFilePath, "json");
-                PluginDescription plugin = GetPluginDescription(jsonFilePath);
-                string pluginDirectory = plugin.ShortName;
-                string pluginInstallPath = Path.Combine(ptInstallationPath, ptInstalledPluginsDirectory, pluginDirectory);
-                /// If this is an upgrade, or a re-install, uninstall the plugin before extracting
-                if (Directory.Exists(pluginInstallPath))
-                    UninstallPlugin(plugin);
-                ZipFile.ExtractToDirectory(pluginArchive.FullName, pluginInstallPath);
-                File.Move(jsonFilePath, pluginInstallPath);
-            }
-            catch (Exception ex)
-            {
-                ReportException(ex);
-            }
+            string zipFilePath = pluginArchive.FullName;
+            string jsonFilePath = Path.ChangeExtension(zipFilePath, "json");
+            PluginDescription plugin = GetPluginDescription(jsonFilePath);
+            string pluginDirectory = plugin.ShortName.ToUpper();
+            string pluginInstallPath = Path.Combine(PtInstalledPluginsPath, pluginDirectory);
+            /// If this is an upgrade, or a re-install, uninstall the plugin before extracting
+            if (Directory.Exists(pluginInstallPath))
+                UninstallPlugin(plugin);
+            ZipFile.ExtractToDirectory(pluginArchive.FullName, pluginInstallPath);
+            string targetJsonFilePath = Path.Combine(pluginInstallPath, Path.GetFileName(jsonFilePath));
+            File.Move(jsonFilePath, targetJsonFilePath);
+            File.Delete(pluginArchive.FullName);
         }
+
+        /// <summary>
+        /// Uninstalls a ParaText plugin.
+        /// </summary>
+        /// <param name="plugin">The plugin to uninstall.</param>
         public void UninstallPlugin(PluginDescription plugin)
         {
-            if (NeedsElevatedPermissions())
-                ElevatePermissions();
+            string pluginInstallPath = Path.Combine(PtInstalledPluginsPath, plugin.ShortName.ToUpper());
+            if (Directory.Exists(pluginInstallPath))
+                Directory.Delete(pluginInstallPath, true);
+        }
 
-            try
-            {
-                string pluginInstallPath = $"{ptInstallationPath}\\{ptInstalledPluginsDirectory}\\{plugin.ShortName.ToUpper()}";
-                if (Directory.Exists(pluginInstallPath))
-                    Directory.Delete(pluginInstallPath);
-            }
-            catch (Exception ex)
-            {
-                ReportException(ex);
-            }
-        }
-        private bool NeedsElevatedPermissions()
-        {
-            return false;
-        }
-        private void ElevatePermissions()
-        {
-            ProcessStartInfo info = new ProcessStartInfo(Process.GetCurrentProcess().MainModule.FileName);
-            info.Verb = "runas";
-            Process.Start(info);
-            Process.Start(info);
-        }
+        /// <summary>
+        /// Gets plugin descriptions in a directory.
+        /// </summary>
+        /// <param name="directory">The directory to search for plugin descriptions.</param>
+        /// <returns>A list of plugin descriptions.</returns>
         public static List<PluginDescription> GetPluginDescriptions(string directory)
         {
             List<PluginDescription> pluginDescriptions = new List<PluginDescription>();
 
-            try
-            {
-                string[] pluginDescriptionFilePaths = Directory.GetFiles(directory, "*.json");
-                foreach (string filePath in pluginDescriptionFilePaths)
-                    pluginDescriptions.Add(GetPluginDescription(filePath));
-            }
-            catch (Exception ex)
-            {
-                ReportException(ex);
-            }
+            string[] pluginDescriptionFilePaths = Directory.GetFiles(directory, "*.json");
+            foreach (string filePath in pluginDescriptionFilePaths)
+                pluginDescriptions.Add(GetPluginDescription(filePath));
 
             return pluginDescriptions;
         }
-        /// <summary>
-        /// Returns the ParaText installation directory
-        /// </summary>
-        /// <returns>The directory where ParaText is installed</returns>
-        public static string GetPTInstallationPath()
+
+        public static PluginDescription GetPluginDescription(string filePath)
         {
-            string sixtyFourBitPath = "WOW6432Node\\";
-            string ptVersion = "8";
-            string subKey = $"SOFTWARE\\{sixtyFourBitPath}Paratext\\{ptVersion}";
-            string name = $"Program_Files_Directory_Ptw{ptVersion}";
+            string rawPluginDescription = File.ReadAllText(filePath);
+            PluginDescription pluginDescription = JsonConvert.DeserializeObject<PluginDescription>(rawPluginDescription);
 
-            RegistryService registryService = new RegistryService();
-            RegistryKey key = registryService.ReadLocalMachineSubKey(subKey);
-            char[] toTrim = { '\\' };
-            return registryService.ReadKeyValue(key, name).ToString().TrimEnd(toTrim);
-        }
-
-        public static void ReportException(Exception ex)
-        {
-            // Variables for tracking error information.
-            IDictionary<string, string> errorDetails = new Dictionary<string, string>();
-            string message = ex.Message;
-
-            // Report the error
-            /// PpmMain.ParatextPluginManagerPlugin.ReportErrorWithDetails(message, errorDetails);
+            return pluginDescription;
         }
 
         public static PluginDescription GetPluginDescription(string filePath)

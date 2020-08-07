eaa04b44b46fbd1969180ae628e23896b27f7c5d
Christian M Macy <christian.macy@opsysinc.com>
2020-08-07 14:50:52 -0600

    DT468: Add `PluginManagerMainFormController` and interface

---
+++ Controllers/IPluginManagerMainFormController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using PpmMain.Models;
+
+namespace PpmMain.Controllers
+{
+    interface IPluginManagerMainFormController
+    {
+        /// <summary>
+        /// A list of plugins that are available to be installed.
+        /// </summary>
+        List<PluginDescription> availablePlugins { get; set; }
+
+        /// <summary>
+        /// A list of plugins that have been installed and for which there are available updates.
+        /// </summary>
+        List<PluginDescription> outdatedPlugins { get; set; }
+
+        /// <summary>
+        /// A list of plugins that have been installed.
+        /// </summary>
+        List<PluginDescription> installedPlugins { get; set; }
+
+        /// <summary>
+        /// A <c>string</c> that limits the plugins that are displayed in a list.
+        /// </summary>
+        string filterCriteria { get; set; }
+
+        /// <summary>
+        /// This method installs a given plugin.
+        /// </summary>
+        /// <param name="plugin">The plugin to install.</param>
+        public void InstallPlugin(PluginDescription plugin);
+
+        /// <summary>
+        /// This method uninstalls a given plugin.
+        /// </summary>
+        /// <param name="plugin">The plugin to uninstall.</param>
+        public void UninstallPlugin(PluginDescription plugin);
+
+        /// <summary>
+        /// This method updates a given list of installed plugins.
+        /// </summary>
+        /// <param name="plugins">A list of installed plugins to update.</param>
+        public void UpdatePlugins(List<PluginDescription> plugins);
+    }
+}
---
+++ Controllers/PluginManagerMainFormController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PpmMain.Models;
+using PpmMain.LocalInstaller;
+using PpmMain.PluginRepository;
+
+namespace PpmMain.Controllers
+{
+    class PluginManagerMainFormController : IPluginManagerMainFormController
+    {
+        public List<PluginDescription> availablePlugins { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<PluginDescription> outdatedPlugins { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<PluginDescription> installedPlugins { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string filterCriteria { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void InstallPlugin(PluginDescription plugin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UninstallPlugin(PluginDescription plugin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePlugins(List<PluginDescription> plugins)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

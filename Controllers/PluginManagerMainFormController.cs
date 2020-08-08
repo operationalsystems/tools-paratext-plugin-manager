84b6704b70e6037003ec9905f9a37dd9d90d9684
Christian M Macy <christian.macy@opsysinc.com>
2020-08-07 19:43:51 -0600

    DT468: Get data from S3 and determine outdated plugins

--- Controllers/PluginManagerMainFormController.cs
+++ Controllers/PluginManagerMainFormController.cs
@@ -1,18 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PpmMain.Models;
 using PpmMain.LocalInstaller;
 using PpmMain.PluginRepository;
 
 namespace PpmMain.Controllers
 {
+    internal class PluginComparer : IEqualityComparer<PluginDescription>
+    {
+        public bool Equals(PluginDescription x, PluginDescription y) => x.Name == y.Name && x.ShortName == y.ShortName;
+
+        public int GetHashCode(PluginDescription obj) => obj.GetHashCode();
+    }
     class PluginManagerMainFormController : IPluginManagerMainFormController
     {
-        public List<PluginDescription> availablePlugins { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public List<OutdatedPlugin> outdatedPlugins { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        IPluginRepository RemotePluginRepository { get; set; }
+
+        IInstallerService LocalInstallerService { get; set; }
+
+        public List<PluginDescription> availablePlugins { get; set; }
+        public List<OutdatedPlugin> outdatedPlugins {
+            get
+            {
+                var outdated = Enumerable.Intersect(installedPlugins, availablePlugins, new PluginComparer());
+
+                return (List<OutdatedPlugin>)outdated.Select(plugin =>
+                {
+                    PluginDescription _availablePlugin = availablePlugins.Find(p => p.Name == plugin.Name && p.ShortName == plugin.ShortName);
+                    return new OutdatedPlugin
+                    {
+                        Name = plugin.Name,
+                        ShortName = plugin.ShortName,
+                        InstalledVersion = plugin.Version,
+                        Version = _availablePlugin.Version,
+                        Description = _availablePlugin.Description,
+                        VersionDescription = _availablePlugin.VersionDescription,
+                        PtVersions = _availablePlugin.PtVersions,
+                        License = _availablePlugin.License
+                    };
+                });
+            }
+            set => throw new NotImplementedException(); 
+        }
         public List<PluginDescription> installedPlugins { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string filterCriteria { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
@@ -29,6 +59,17 @@ namespace PpmMain.Controllers
         public void UpdatePlugins(List<OutdatedPlugin> plugins)
         {
             throw new NotImplementedException();
+        }
+
+        public PluginManagerMainFormController()
+        {
+            string installPath = System.IO.Path.Combine(System.IO.Path.GetTempPath(), "DT468");
+            var onlyLatestPlugins = true;
+
+            RemotePluginRepository = new S3PluginRepositoryService();
+            LocalInstallerService = new LocalInstallerService(installPath);
+            availablePlugins = RemotePluginRepository.GetAvailablePlugins(onlyLatestPlugins);
+            installedPlugins = LocalInstallerService.GetInstalledPlugins();
         }
     }
 }
--- Forms/PluginManagerMainForm.cs
+++ Forms/PluginManagerMainForm.cs
@@ -1,22 +1,24 @@
-﻿using PpmMain.Models;
+﻿using PpmMain.Controllers;
+using PpmMain.LocalInstaller;
+using PpmMain.Models;
 using PpmMain.PluginRepository;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
 
 namespace PpmMain
 {
     public partial class PluginManagerMainForm : Form
     {
-        IPluginRepository RemotePluginRepository { get; set; }
+        IPluginManagerMainFormController Controller { get; set; }
 
         public PluginManagerMainForm()
         {
             InitializeComponent();
 
-            // set up the Plugin Repository Service
-            RemotePluginRepository = new S3PluginRepositoryService();
+            Controller = new PluginManagerMainFormController();
         }
 
         private void PluginManagerMainForm_Load(object sender, EventArgs e)

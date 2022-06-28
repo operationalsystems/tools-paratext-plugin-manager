b382ef8a383b70bbe8cc58d24d29faa29c485a48
Mark-Anthony Hutton <mark-anthony.hutton@opsysinc.com>
2022-06-28 15:02:37 -0600

    DT-461:
    - Add initial event/console logging capability
    - Remove stub form
    - Wire ParatextPluginManagerApp to start on PpmApp run

--- PpmApp/ParatextPluginManagerApp.cs
+++ PpmApp/ParatextPluginManagerApp.cs
@@ -1,22 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
 using System.Windows.Forms;
 
 namespace PpmApp
 {
-    static class ParatextPluginManagerApp
+    class ParatextPluginManagerApp
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main()
         {
+            // set up logging filters
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .AddFilter("Microsoft", LogLevel.Warning)
+                    .AddFilter("System", LogLevel.Warning)
+                    .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                    .AddConsole()
+                    .AddEventLog();
+            });
+
+            ILogger logger = loggerFactory.CreateLogger<ParatextPluginManagerApp>();
+            logger.LogInformation("Example log message");
+
+            // set up UI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PluginManagerMainForm());
         }
     }
 }
